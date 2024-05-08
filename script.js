efba0f60fbfa107f6245bc6bd159d9cc4e152bd7
ParryOgbemudia <parryogbemudia@gmail.com>
2024-05-08 14:44:07 -0700

    more update on inheritance

--- script.js
+++ script.js
@@ -198,7 +198,7 @@ console.log(newCar.speedUS);
 console.log(newCar.accelerator);
 console.log(newCar.break);
 */
-
+/*
 const Person = function (firstName, birthYear) {
   this.firstName = firstName;
   this.birthYear = birthYear;
@@ -213,9 +213,265 @@ const Student = function (firstName, birthYear, course) {
   this.course = course;
 };
 
+//Linking Prototypes
+Student.prototype = Object.create(Person.prototype);
+
 Student.prototype.introduce = function () {
   console.log(`My name is ${this.firstName} and I study ${this.course}`);
 };
 
 const mike = new Student('Mike', 2020, 'Computer Science');
 mike.introduce();
+mike.calcAge();
+
+console.log(mike.__proto__);
+console.log(mike.__proto__.__proto__);
+
+console.log(mike instanceof Student);
+console.log(mike instanceof Person);
+console.log(mike instanceof Object);
+
+Student.prototype.constructor = Student;
+console.dir(Student.prototype.constructor);
+*/
+/*
+const Car = function (make, speed) {
+  this.speed = speed;
+  this.make = make;
+};
+
+Car.prototype.accelerator = function () {
+  console.log(`${this.make} going at ${(this.speed += 10)}km/h`);
+};
+Car.prototype.break = function () {
+  console.log(`${this.make} going at ${(this.speed -= 5)}km/h`);
+};
+
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed, charge);
+  this.charge = charge;
+};
+
+///Linking prototypes
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge -= this.charge * 0.01;
+  console.log(
+    `${this.make} going at ${(this.speed += 10)}km/h, with a charge of ${
+      this.charge
+    }`
+  );
+};
+
+const tesla = new EV('Tesla', 120, 23);
+tesla.chargeBattery(90);
+console.log(tesla);
+tesla.break();
+tesla.accelerate();
+*/
+
+/*
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+  greet() {
+    console.log(`Hey ${this.fullName}`);
+  }
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  set fullName(name) {
+    if (name.includes(' ')) {
+      this._fullName = name;
+    } else {
+      alert(`${name} is not a full name`);
+    }
+  }
+  get fullName() {
+    return this._fullName;
+  }
+  static hey() {
+    console.log('Hey there');
+    console.log(this);
+  }
+}
+class StudentCl extends PersonCl {
+  constructor(fullName, brithYear, course) {
+    //Always needs to happen first
+    super(fullName, brithYear);
+    this.course = course;
+  }
+  introduce() {
+    console.log(`My name is ${this.fullName} and In study is ${this.course}`);
+  }
+  calcAge() {
+    console.log(
+      `I'm ${
+        2037 - this.birthYear
+      } years old, but as a student I feel more like ${
+        2037 - this.birthYear + 10
+      }`
+    );
+  }
+}
+
+// const martha = new StudentCl('Martha Jones', 2012);
+const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
+
+martha.introduce();
+martha.calcAge();
+*/
+/*
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.fullName} and In study is ${this.course}`);
+};
+
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2010, 'Computer Science');
+jay.introduce();
+jay.calcAge();
+console.log(jay);
+
+*/
+/*
+// 1) public fileds
+// 2) private fields
+// 3) public method
+// 4) private meyhod
+
+class Account {
+  //public fields instances
+  locale = navigator.language;
+  #movements = [];
+  #pin;
+
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+
+    //protected property
+    this.#pin = pin;
+    // this._movements = [];
+    // this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+  //Public interface
+  getMovements() {
+    return this.#movements;
+  }
+
+  deposit(val) {
+    this.#movements.push(val);
+    return this;
+  }
+
+  withdrawal(val) {
+    this.deposit(-val);
+    return this;
+  }
+  //private method
+  _approvedLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    // if (this.#approvedLoan(val)) {
+    if (this._approvedLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved: ${this._approvedLoan(val)}`);
+      return this;
+    }
+  }
+
+  static herper() {
+    console.log('Static method');
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111);
+
+acc1.deposit(250);
+acc1.withdrawal(140);
+acc1.requestLoan(1000);
+
+console.log(acc1.getMovements());
+Account.herper();
+// console.log(acc1.#pin);
+// console.log(acc1.#approvedLoan(1000));
+
+//Chaining
+acc1
+  .deposit(300)
+  .deposit(500)
+  .withdrawal(35)
+  .requestLoan(25000)
+  .withdrawal(4000);
+
+console.log(acc1.getMovements());
+
+*/
+
+const Car = function (make, speed) {
+  this.speed = speed;
+  this.make = make;
+};
+
+Car.prototype.accelerator = function () {
+  console.log(`${this.make} going at ${(this.speed += 10)}km/h`);
+};
+Car.prototype.break = function () {
+  console.log(`${this.make} going at ${(this.speed -= 5)}km/h`);
+};
+
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed, charge);
+  this.charge = charge;
+};
+
+///Linking prototypes
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge -= this.charge * 0.01;
+  console.log(
+    `${this.make} going at ${(this.speed += 10)}km/h, with a charge of ${
+      this.charge
+    }`
+  );
+};
